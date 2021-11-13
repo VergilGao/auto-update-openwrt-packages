c03abdb0d5df3688538a0d483ec70578a0a673dc
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-11-13 19:43:54 +0800

    删除不需要的软件包

--- 21.02.sh
+++ 21.02.sh
@@ -10,21 +10,11 @@ for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
 done
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr
 sed -i "s/wget-ssl/wget/g" `grep wget-ssl -rl ./luci-app-vssr`
-# OpenClash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./luci-app-openclash
-# 京东签到
-svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk ./luci-app-jd-dailybonus
 # argon主题（适配最新版本luci）
 svn co https://github.com/jerrykuku/luci-theme-argon/trunk ./luci-theme-argon
 svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-app-argon-config
 # 基于DNS的广告过滤
 svn co https://github.com/small-5/luci-app-adblock-plus/trunk ./luci-app-adblock-plus
-# kms
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
-# uu加速器
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/uugamebooster
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-uugamebooster
 # ddns_pod
 svn co https://github.com/VergilGao/ddns-scripts-dnspod/trunk ./ddns-scripts-dnspod
 
